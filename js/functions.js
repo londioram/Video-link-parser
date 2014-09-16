56c0ecdd8da1b584169b8daa7eaf9f749a0fbbc3
Londio <londioram@gmail.com>
2014-09-16 15:16:05 +0300

    Upload with fixes
    Added checking and description.

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,9 @@
+/**
+ *  This script process a direct link of video from video hosting:
+ *      YouTube, RuTube, Vimeo, DailyMotion, Twitch, Coub ...
+ *  And generate iframe for including video into your website.
+**/
+
 function parseURL(url) {
     var a =  document.createElement('a');
     a.href = url;
@@ -31,76 +37,95 @@ function generateIFrame()
     deleteIFrame();
 
     var url = parseURL(document.getElementById("video_url").value);
-
+    /**YouTube**/
     if (url.host == "www.youtube.com" || url.host == "youtube.com")
     {
-        alert("Video server: " + url.host + "\n" + "Video id:     " + url.params["v"]);
-        var iframe = '<iframe src="https://' + url.host + '/embed/' + url.params["v"] + '?rel=0" frameborder="0" allowfullscreen></iframe>';
-        document.getElementById("embedded_container").innerHTML = iframe;
-        return iframe;
+        if (url.params["v"] == undefined)
+        {
+            return incorrectID(url);
+        }
+        else
+        {
+            var iframe = '<iframe src="https://' + url.host + '/embed/' + url.params["v"] + '?rel=0" frameborder="0" allowfullscreen></iframe>';
+            document.getElementById("embedded_container").innerHTML = iframe;
+            return iframe;
+        }
     }
+    /**RuTube**/
     else if (url.host == "www.rutube.ru" || url.host == "rutube.ru")
     {
-        alert("Video server: " + url.host + "\n" + "Video id:     " + url.segments[1]);
-        var iframe = '<iframe src="http://' + url.host + '/play/embed/' + url.segments[1] + '" frameborder="0" allowfullscreen></iframe>';
-        document.getElementById("embedded_container").innerHTML = iframe;
-        return iframe;
+        if (url.segments[0] != "video")
+        {
+            return incorrectID(url);
+        }
+        else
+        {
+            var iframe = '<iframe src="http://' + url.host + '/play/embed/' + url.segments[1] + '" frameborder="0" allowfullscreen></iframe>';
+            document.getElementById("embedded_container").innerHTML = iframe;
+            return iframe;
+        }
     }
+    /**Vimeo**/
     else if (url.host == "www.vimeo.com" || url.host == "vimeo.com")
     {
         if (url.segments[0] == "channels")
         {
-            alert("Video server: " + url.host + "\n" + "Video id:     " + url.segments[2]);
             var iframe = '<iframe src="http://player.' + url.host + '/video/' + url.segments[2] + '" frameborder="0" allowfullscreen></iframe>';
             document.getElementById("embedded_container").innerHTML = iframe;
             return iframe;
         }
         else if (url.segments[0] != "channels")
         {
-            alert("Video server: " + url.host + "\n" + "Video id:     " + url.segments[0]);
             var iframe = '<iframe src="http://player.' + url.host + '/video/' + url.segments[0] + '" frameborder="0" allowfullscreen></iframe>';
             document.getElementById("embedded_container").innerHTML = iframe;
             return iframe;
         }
-        //else
-        //{
-        //    alert("Video server error: " + url.host);
-        //    var iframe = '<div class="alert alert-danger" role="alert">Sorry, but we can not process video from ' + url.host + '. Maybee video ID is incorrect! ' + url.segments[2] + '</div>';
-        //    document.getElementById("embedded_container").innerHTML = iframe;
-        //    return iframe;
-        //}
     }
+    /**DailyMotion**/
     else if (url.host == "www.dailymotion.com" || url.host == "dailymotion.com")
     {
-        alert("Video server: " + url.host + "\n" + "Video id:     " + url.segments[1]);
-        var iframe = '<iframe src="http://' + url.host + '/embed/video/' + url.segments[1] + '" frameborder="0" allowfullscreen></iframe>';
-        document.getElementById("embedded_container").innerHTML = iframe;
-        return iframe;
+        if (url.segments[0] != "video")
+        {
+            return incorrectID(url);
+        }
+        else
+        {
+            var iframe = '<iframe src="http://' + url.host + '/embed/video/' + url.segments[1] + '" frameborder="0" allowfullscreen></iframe>';
+            document.getElementById("embedded_container").innerHTML = iframe;
+            return iframe;
+        }
     }
+    /**Twitch**/
     else if (url.host == "www.twitch.tv" || url.host == "twitch.tv")
     {
         if (url.segments[1] == "c")
         {
-            alert("Video server: " + url.host + "\n" + "Video id:     " + url.segments[2]);
             var iframe = '<iframe src="http://' + url.host + '/widgets/live_embed_player.swf?channel=' + url.query + '" frameborder="0" scrolling="no" allowfullscreen></iframe>';
             document.getElementById("embedded_container").innerHTML = iframe;
             return iframe;
         }
         else
         {
-            alert("Video server: " + url.host + "\n" + "Video id:     " + url.segments[0]);
             var iframe = '<iframe src="http://' + url.host + '/widgets/live_embed_player.swf?channel=' + url.segments[0] + '" frameborder="0" scrolling="no" allowfullscreen></iframe>';
             document.getElementById("embedded_container").innerHTML = iframe;
             return iframe;
         }
     }
+    /**Coub**/
     else if (url.host == "www.coub.com" || url.host == "coub.com")
     {
-        alert("Video server: " + url.host + "\n" + "Video id:     " + url.segments[1]);
-        var iframe = '<iframe src="http://' + url.host + '/embed/' + url.segments[1] + '" frameborder="0" allowfullscreen></iframe>';
-        document.getElementById("embedded_container").innerHTML = iframe;
-        return iframe;
+        if (url.segments[0] != "view")
+        {
+            return incorrectID(url);
+        }
+        else
+        {
+            var iframe = '<iframe src="http://' + url.host + '/embed/' + url.segments[1] + '" frameborder="0" allowfullscreen></iframe>';
+            document.getElementById("embedded_container").innerHTML = iframe;
+            return iframe;
+        }
     }
+
     else
     {
         alert("Video server error! ");
@@ -113,5 +138,12 @@ function generateIFrame()
 function deleteIFrame()
 {
     document.getElementById("embedded_container").innerHTML = "<!--Place for iframe-->";
+}
+
+function incorrectID(url)
+{
+    var iframe = '<div class="alert alert-danger" role="alert">Sorry! But we can not process this video from <strong>' + url.host + '</strong> Maybee video\'s ID is incorrect! </div>';
+    document.getElementById("embedded_container").innerHTML = iframe;
+    return iframe;
 }
 
